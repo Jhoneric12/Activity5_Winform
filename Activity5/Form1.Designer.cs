4f139ed6231d26ed0caf0477c1a7cc29e082916e
Jhon Eric <jhonerix23@gmail.com>
2022-03-21 08:50:08 -0500

    first commit

---
+++ Activity5/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Activity5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.cmbo_BoxItems = new System.Windows.Forms.ComboBox();
+            this.lBoxItems = new System.Windows.Forms.ListBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnRemove);
+            this.panel1.Controls.Add(this.btnAdd);
+            this.panel1.Controls.Add(this.cmbo_BoxItems);
+            this.panel1.Controls.Add(this.lBoxItems);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(562, 450);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRemove.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemove.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnRemove.Location = new System.Drawing.Point(313, 280);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(207, 54);
+            this.btnRemove.TabIndex = 3;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnAdd.Location = new System.Drawing.Point(313, 197);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(207, 54);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // cmbo_BoxItems
+            // 
+            this.cmbo_BoxItems.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(25)))), ((int)(((byte)(53)))));
+            this.cmbo_BoxItems.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbo_BoxItems.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmbo_BoxItems.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbo_BoxItems.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.cmbo_BoxItems.FormattingEnabled = true;
+            this.cmbo_BoxItems.Location = new System.Drawing.Point(313, 90);
+            this.cmbo_BoxItems.Name = "cmbo_BoxItems";
+            this.cmbo_BoxItems.Size = new System.Drawing.Size(207, 30);
+            this.cmbo_BoxItems.TabIndex = 1;
+            // 
+            // lBoxItems
+            // 
+            this.lBoxItems.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(25)))), ((int)(((byte)(53)))));
+            this.lBoxItems.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lBoxItems.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.lBoxItems.FormattingEnabled = true;
+            this.lBoxItems.ItemHeight = 26;
+            this.lBoxItems.Items.AddRange(new object[] {
+            "Pants",
+            "Skirts",
+            "T-Shirts",
+            "Sleeveless",
+            "Shorts",
+            "Hoodie",
+            "Polo Shirt"});
+            this.lBoxItems.Location = new System.Drawing.Point(35, 53);
+            this.lBoxItems.Name = "lBoxItems";
+            this.lBoxItems.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.lBoxItems.Size = new System.Drawing.Size(221, 316);
+            this.lBoxItems.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(25)))), ((int)(((byte)(53)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(562, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "List Box";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ComboBox cmbo_BoxItems;
+        private System.Windows.Forms.ListBox lBoxItems;
+    }
+}
+
---
+++ Activity5/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            // Background
+            panel1.BackColor = Color.FromArgb(90, 0, 0, 0);
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            for (int i = lBoxItems.SelectedIndices.Count - 1; i >= 0; i--)
+            {
+                cmbo_BoxItems.Items.Add(lBoxItems.SelectedItems[i]);
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            cmbo_BoxItems.Items.RemoveAt(cmbo_BoxItems.SelectedIndex);
+        }
+    }
+}
